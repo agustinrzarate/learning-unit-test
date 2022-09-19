69d71fb7e0ad9874207fd185bf5de2fb51cb0151
Agustin R. Zarate <agustinrzarate@gmail.com>
2022-09-19 10:44:39 -0300

    Created button and testing your changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('red')
+  const [enableButton, setEnableButton] = useState(false)
+  const changeColor = color === 'red' ? 'blue' : 'red'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <button style={{background: color}}  onClick={() => setColor(changeColor)} disabled={enableButton}>Change to {changeColor}</button>
+      <div>
+        <label htmlFor="disable-button-checkbox">Disable button</label>
+        <input id="disable-button-checkbox" type="checkbox" checked={enableButton} onChange={() => setEnableButton(!enableButton)}/>
+      </div>
+      
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Testing button', () => {
+  const user = userEvent.setup()
+  
+
+  it('Button has correct initial color', () => {
+      render(<App />)
+      const button = screen.getByRole('button', {name: 'Change to blue'})
+      expect(button).toHaveStyle({background: 'red'})
+  });
+
+  it('Should click button and change color background and text', async () => {
+    render(<App />)
+    const button = screen.getByRole('button', {name: 'Change to blue'})
+    await user.click(button)
+    expect(button).toHaveStyle({background: 'blue'})
+    expect(button).toHaveTextContent('Change to red')
+  })
+
+  it('Should click on checkbox', async () => {
+    render(<App />)
+    const button = screen.getByRole('button')
+    const checkbox = screen.getByRole('checkbox', { name: 'Disable button'})
+    
+    expect(button).not.toBeDisabled()
+    expect(checkbox).not.toBeChecked()
+    await user.click(checkbox)
+    expect(button).toBeDisabled()
+
+  })
+})
+
